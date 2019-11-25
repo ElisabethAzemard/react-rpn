b203b5c10e0f6b6e17d7edb4e25a63d8b5630d61
Elisabeth Azémard <elie.azemard@gmail.com>
2019-11-25 21:03:26 +0100

    Add calculator component skeleton.

---
+++ src/components/Calculator.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: []
+        }
+    }
+
+    render() {
+
+        return (
+            <div>My awesome calculator!</div>
+        )
+    }
+
+}
+
+export default Calculator;
